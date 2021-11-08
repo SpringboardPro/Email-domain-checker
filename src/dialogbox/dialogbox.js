9dedc42c78ccdd7349c21aa5bde068089770954f
Hamish-Atkins-SB <68431164+Hamish-Atkins-SB@users.noreply.github.com>
2021-11-08 09:56:03 +0000

    Update dialogbox.js

--- src/dialogbox/dialogbox.js
+++ src/dialogbox/dialogbox.js
@@ -2,24 +2,26 @@ Office.onReady().then(()=> {
         //console.log(Office.context.ui)
         Office.context.ui.messageParent("READY")
         //OFFICE MIGHT NOT BE READY BY THE TIME IT TRIES TO SEND INFORMATION TO THE DIALOG- GET DIALOG TO SEND BACK FIRST THAT IT IS READY THEN USE meesageChild
-        console.log('in dialog')
-        const form = document.querySelector('form');
-        form.addEventListener('submit', e => {
-                e.preventDefault();
-
-                  const values = Array.from(document.querySelectorAll('input[type=checkbox]:checked'))
-                    .map(item => item.value)
-                    .join(',');
-
-                console.log(`?${values}`);
-
-                });
+        console.log('in dialog')    
         
-        
-        
+        //Recieve emails from host page
         Office.context.ui.addHandlerAsync(
             Office.EventType.DialogParentMessageReceived,
             onMessageFromParent);     
+        
+        
+        
+        //Get form results from dialog box
+        const form = document.querySelector('form');
+        form.addEventListener('submit', e => {
+                e.preventDefault();
+                  const values = Array.from(document.querySelectorAll('input[type=checkbox]:checked'))
+                    .map(item => item.value)
+                    .join(',');
+                console.log(`${values}`);
+                });
+        
+        
     });
 
 function onMessageFromParent(arg){
