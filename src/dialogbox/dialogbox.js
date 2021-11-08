f3e8f7be2aaf16cc2352114e28aa40ba03abf458
Hamish-Atkins-SB <68431164+Hamish-Atkins-SB@users.noreply.github.com>
2021-11-08 09:48:37 +0000

    Update dialogbox.js

--- src/dialogbox/dialogbox.js
+++ src/dialogbox/dialogbox.js
@@ -3,6 +3,19 @@ Office.onReady().then(()=> {
         Office.context.ui.messageParent("READY")
         //OFFICE MIGHT NOT BE READY BY THE TIME IT TRIES TO SEND INFORMATION TO THE DIALOG- GET DIALOG TO SEND BACK FIRST THAT IT IS READY THEN USE meesageChild
         console.log('in dialog')
+        form.addEventListener('submit', e => {
+                e.preventDefault();
+
+                  const values = Array.from(document.querySelectorAll('input[type=checkbox]:checked'))
+                    .map(item => item.value)
+                    .join(',');
+
+                console.log(`?${values}`);
+                });
+});
+        
+        
+        
         Office.context.ui.addHandlerAsync(
             Office.EventType.DialogParentMessageReceived,
             onMessageFromParent);     
