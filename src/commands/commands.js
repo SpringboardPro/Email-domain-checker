84ce742d02a5293c18c27c410a033da3fb76196a
Hamish-Atkins-SB <68431164+Hamish-Atkins-SB@users.noreply.github.com>
2021-11-08 11:18:30 +0000

    Update commands.js

--- src/commands/commands.js
+++ src/commands/commands.js
@@ -32,7 +32,7 @@ function openDialog(event) {
 
           dialog = asyncResult.value;
           
-          dialog.addEventHandler(Office.EventType.DialogMessageReceived, dialog.messageChild(JSON.stringify(recipients, { targetOrigin: "*" }));
+          dialog.addEventHandler(Office.EventType.DialogMessageReceived, dialog.messageChild(JSON.stringify(recipients), { targetOrigin: "*" }));
                     
         });
     }
