0557e386b36bbfc99d8609c30bc0b3cc0e2417c8
Hamish-Atkins-SB <68431164+Hamish-Atkins-SB@users.noreply.github.com>
2021-11-09 17:03:33 +0000

    Update commands.js

--- src/commands/commands.js
+++ src/commands/commands.js
@@ -22,6 +22,7 @@ function openDialog(event) {
   var promise3 = Promise.all([promise1, promise2]).then(function(result){
     all_recipient_data = result
     recipients = getRecipients(result)
+    print(recipients)
     return recipients
   })
 
