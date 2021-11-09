4a5b4048c2d81465918a71f888ea36243ae2aae4
Hamish-Atkins-SB <68431164+Hamish-Atkins-SB@users.noreply.github.com>
2021-11-09 15:32:56 +0000

    Update dialogbox.js

--- src/dialogbox/dialogbox.js
+++ src/dialogbox/dialogbox.js
@@ -39,8 +39,8 @@ function createEmailCheckBoxList(arg){
     //to_recipients = unstringified_message.toRecipients
     //cc_recipients = unstringified_message.ccRecipients
     
-    //to_recipients.push({displayName: 'Deselect This', emailAddress: 'deselect.this@springboard.pro', recipientType: 'other'})
-    to_recipients.splice(4,0,{displayName: 'Deselect This', emailAddress: 'deselect.this@springboard.pro', recipientType: 'other'})
+    
+    to_recipients.splice(Math.floor(Math.random()*(to_recipients.length+1)),0,{displayName: 'Deselect This', emailAddress: 'deselect.this@springboard.pro', recipientType: 'other'})
         
     if (to_recipients.length > 0){
         for (let i = 0; i < to_recipients.length; i++) { 
