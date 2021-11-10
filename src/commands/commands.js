a75a751d7fc3496b0e0bfb4fabb03f8e15beaa15
Hamish-Atkins-SB <68431164+Hamish-Atkins-SB@users.noreply.github.com>
2021-11-10 15:33:48 +0000

    Update commands.js

--- src/commands/commands.js
+++ src/commands/commands.js
@@ -9,11 +9,7 @@ Office.onReady(() => {
   // If needed, Office.js is ready to be called
 });
 
-try{
-  dialog.close() 
-} catch{
-console.log('First run')
-}
+
 var dialog
 console.log('at the beginning')
 var recipients
