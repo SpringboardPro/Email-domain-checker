8ec3a75eb24f896a8f88beb7f1cea37eee237236
Hamish-Atkins-SB <68431164+Hamish-Atkins-SB@users.noreply.github.com>
2021-12-21 11:13:49 +0000

    Updated reference URLs due to move of repo to Springboard Pro

--- src/commands/commands.js
+++ src/commands/commands.js
@@ -45,7 +45,7 @@ function openDialog(event) {
       event.completed({allowEvent: true});
     } else {
       //Display dialog box (callback function in dialog is to create event handler in host page to recieve info from dialog page).
-      var url ='https://hamish-atkins-sb.github.io/Email-domain-checker/src/dialogbox/dialogbox.html'
+      var url ='https://springboardpro.github.io/Email-domain-checker/src/dialogbox/dialogbox.html'
       console.log(Office.context.ui)
       Office.context.ui.displayDialogAsync(url, {height: 50, width: 50, displayInIframe: true}, 
         function (asyncResult) {
